7f0b3cc09c9515b2d54e5377c12dc61070bb4176
Steve Kehlet <steve.kehlet@gmail.com>
2014-11-15 19:42:39 -0800

    custom 404 to redirect subnet.php visitors

--- docs/2004/03/subnet.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<title>Redirecting...</title>
-<link rel=canonical href="http://www.kehlet.cx/subnetting-is-fun/index.html">
-<meta http-equiv=refresh content="0; url=http://www.kehlet.cx/subnetting-is-fun/index.html">
-<h1>Redirecting...</h1>
-<a href="http://www.kehlet.cx/subnetting-is-fun/index.html">Click here if you are not redirected.</a>
-<script>location='http://www.kehlet.cx/subnetting-is-fun/index.html'</script>
