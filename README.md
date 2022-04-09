b82e6cd18593ff662a0a89d07f6bcd53eaff20cd
Steve Kehlet <skehlet@nextgen.com>
2022-04-08 17:10:23 -0700

    retiring the blog

--- README.md
+++
@@ -1,4 +0,0 @@
-www.kehlet.cx
-=============
-
-This is my www.kehlet.cx web site/blog, hosted on GitHub Pages.
--- subnetting-is-fun/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.version',
-  'myApp.autoFocus'
-])
